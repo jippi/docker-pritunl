211e21ed4452f6a39733622d57cc5b406b52bf45
Christian Winther <jippignu@gmail.com>
2018-09-05 13:27:21 +0200

    bump
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- docker-install.sh
+++ docker-install.sh
@@ -5,8 +5,12 @@ if [ "${MONGODB_VERSION}" != "no" ]; then
     apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 42F3E95A2C4F08279C4960ADD68FA50FEA312927
 fi
 
+echo 'deb http://repo.pritunl.com/stable/apt xenial main' > /etc/apt/sources.list.d/pritunl.list
 echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 8E6DA8B4E158C569
+
 apt-get update -q
 apt-get install -y locales iptables wget
 locale-gen en_US en_US.UTF-8
@@ -16,7 +20,8 @@ apt-get upgrade -y -q
 apt-get dist-upgrade -y -q
 
 wget --quiet https://github.com/pritunl/pritunl/releases/download/${PRITUNL_VERSION}/pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb
-dpkg -i pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb
+dpkg -i pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb || exit 0
+apt-get -f install
 rm pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb
 
 if [ "${MONGODB_VERSION}" != "no" ]; then
