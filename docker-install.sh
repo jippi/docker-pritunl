49055fd8fa66ec95ee4cd025316d022ab890e158
Christian Winther <jippignu@gmail.com>
2018-09-05 12:35:35 +0200

    more release work
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- docker-install.sh
+++ docker-install.sh
@@ -5,24 +5,25 @@ if [ "${MONGODB_VERSION}" != "no" ]; then
     apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 42F3E95A2C4F08279C4960ADD68FA50FEA312927
 fi
 
-echo 'deb http://repo.pritunl.com/stable/apt xenial main' > /etc/apt/sources.list.d/pritunl.list
 echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
-
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 8E6DA8B4E158C569
 apt-get update -q
-apt-get install locales
+apt-get install -y locales iptables wget
 locale-gen en_US en_US.UTF-8
 dpkg-reconfigure locales
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 apt-get upgrade -y -q
 apt-get dist-upgrade -y -q
-apt-get -y install pritunl=${PRITUNL_VERSION} iptables
+
+wget --quiet https://github.com/pritunl/pritunl/releases/download/${PRITUNL_VERSION}/pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb
+dpkg -i pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb
+rm pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb
 
 if [ "${MONGODB_VERSION}" != "no" ]; then
     apt-get -y install mongodb-org=${MONGODB_VERSION};
 fi
 
+apt-get purge wget
 apt-get clean
 apt-get -y -q autoclean
 apt-get -y -q autoremove
--- update-pritunl.sh
+++ update-pritunl.sh
@@ -1,7 +1,16 @@
-set -ex
+set -e
+set -o pipefail
 
-# find latest tag from github
-tag=$(curl https://api.github.com/repos/pritunl/pritunl/tags | jq -r '.[0].name')
+DOCKER_ARGS="--squash"
+DEBUG=${DEBUG:0}
+
+if [ "${DEBUG}" != "1" ]; then
+    DOCKER_ARGS="$DOCKER_ARGS --quiet"
+fi
+
+if [ "${DEBUG}" == "1" ]; then
+    set -x
+fi
 
 # ensure checkout directory exist
 if [ ! -d "/tmp/docker-pritunl" ]; then
@@ -9,21 +18,71 @@ if [ ! -d "/tmp/docker-pritunl" ]; then
 fi
 
 # change work dir
-cd /tmp/docker-pritunl/
+# cd /tmp/docker-pritunl/
 
 # update repo
-git pull
+# git pull
 
-# build without mongo (default container)
-docker build -t "jippi/pritunl:${tag}" --build-arg PRITUNL_VERSION="${tag}*" --build-arg MONGODB_VERSION=no .
-docker push "jippi/pritunl:${tag}"
+# docker tags
+docker_tags=$(curl -s https://hub.docker.com/v2/repositories/jippi/pritunl/tags/ | jq -r '.results[].name')
+docker_tags=""
 
-docker tag "jippi/pritunl:${tag}" "jippi/pritunl:latest"
-docker push "jippi/pritunl:latest"
+function has_tag() {
+    check=$(echo "${docker_tags}" | grep "^$1$")
+    if [ "${check}" == "" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
 
-# build with mongo (special tag)
-docker build -t "jippi/pritunl:${tag}-mongo" --build-arg PRITUNL_VERSION="${tag}*" .
-docker push "jippi/pritunl:${tag}-mongo"
+# find latest tag from github
+github_tags=$(curl -s https://api.github.com/repos/pritunl/pritunl/tags | jq -r '.[].name')
+first=1
+for tag in $github_tags; do
+    echo "[${tag}] Processing"
 
-docker tag "jippi/pritunl:${tag}" "jippi/pritunl:latest-mongo"
-docker push "jippi/pritunl:latest-mongo"
+    # build with mongo (default container)
+    if ! has_tag "${tag}"; then
+        echo "[${tag}] Building"
+        docker build $DOCKER_ARGS -t "jippi/pritunl:${tag}-minimal" --build-arg PRITUNL_VERSION="${tag}" .
+
+        echo "[${tag}] Pushing"
+        docker push "jippi/pritunl:${tag}"
+
+        if [ $first -eq 1 ]; then
+            echo "[${tag}] Tagging as latest"
+            docker tag "jippi/pritunl:${tag}" "jippi/pritunl:latest"
+
+            echo "[${tag}] Pushing as latest"
+            docker push "jippi/pritunl:latest"
+        fi
+
+        echo "[${tag}] Done"
+    else
+        echo "[${tag}] Already build"
+    fi
+
+    # build without mongo (special tag)
+    if ! has_tag "${tag}-minimal"; then
+        echo "[${tag}-minimal] Building"
+        docker build $DOCKER_ARGS -t "jippi/pritunl:${tag}" --build-arg PRITUNL_VERSION="${tag}" --build-arg MONGODB_VERSION=no .
+
+        echo "[${tag}-minimal] Pushing"
+        docker push "jippi/pritunl:${tag}-minimal"
+
+        if [ $first -eq 1 ]; then
+            echo "[${tag}-minimal] Tagging as latest-minimal"
+            docker tag "jippi/pritunl:${tag}-minimal" "jippi/pritunl:latest-minimal"
+
+            echo "[${tag}-minimal] Pushing as latest-minimal"
+            docker push "jippi/pritunl:latest-minimal"
+        fi
+
+        echo "[${tag}-minimal] Done"
+    else
+        echo "[${tag}-minimal] Already build"
+    fi
+
+    first=0
+done
