5f738bd90d13b4c34108921b462689736b3aa68b
Christian Winther <jippignu@gmail.com>
2021-09-30 06:05:06 +0000

    bionic
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- docker-install.sh
+++ docker-install.sh
@@ -1,12 +1,15 @@
 set -ex
 
+apt-get update -q
+apt-get install -y gnupg wget
+
 if [ "${MONGODB_VERSION}" != "no" ]; then
-    echo 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse' > /etc/apt/sources.list.d/mongodb-org-3.2.list
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 42F3E95A2C4F08279C4960ADD68FA50FEA312927
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 fi
 
-echo 'deb http://repo.pritunl.com/stable/apt xenial main' > /etc/apt/sources.list.d/pritunl.list
-echo "deb http://build.openvpn.net/debian/openvpn/stable xenial main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
+echo 'deb http://repo.pritunl.com/stable/apt bionic main' > /etc/apt/sources.list.d/pritunl.list
+echo "deb http://build.openvpn.net/debian/openvpn/stable bionic main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 8E6DA8B4E158C569
@@ -19,9 +22,9 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 apt-get upgrade -y -q
 apt-get dist-upgrade -y -q
 
-wget --quiet https://github.com/pritunl/pritunl/releases/download/${PRITUNL_VERSION}/pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb
-dpkg -i pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb || apt-get -f -y install
-rm pritunl_${PRITUNL_VERSION}-0ubuntu1.xenial_amd64.deb
+wget --quiet https://github.com/pritunl/pritunl/releases/download/${PRITUNL_VERSION}/pritunl_${PRITUNL_VERSION}-0ubuntu1.bionic_amd64.deb
+dpkg -i pritunl_${PRITUNL_VERSION}-0ubuntu1.bionic_amd64.deb || apt-get -f -y install
+rm pritunl_${PRITUNL_VERSION}-0ubuntu1.bionic_amd64.deb
 
 if [ "${MONGODB_VERSION}" != "no" ]; then
     apt-get -y install mongodb-org=${MONGODB_VERSION};
